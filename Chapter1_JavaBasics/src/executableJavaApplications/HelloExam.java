69d8f84cdec3ce29c6510eef9c431f9895c2afc4
madus <madalin.mihai.udrea@gmail.com>
2022-05-29 18:31:09 +0300

    Oca Certification Guide Chapter 1 -Java Basics

---
+++ Chapter1_JavaBasics/src/executableJavaApplications/HelloExam.java
@@ -0,0 +1,10 @@
+package executableJavaApplications;
+
+/**
+ * Created by Alexandra on 5/29/2022.
+ */
+public class HelloExam {
+    public static void main(String[] args) {
+        System.out.println("Hello Exam");
+    }
+}
---
+++ Chapter1_JavaBasics/src/twist_in_the_tale_exercise/Multiple.java
@@ -0,0 +1,20 @@
+package twist_in_the_tale_exercise;
+
+/**
+ * Created by Alexandra on 5/29/2022.
+ */
+public class Multiple {
+
+
+
+}
+
+interface Printable{
+
+
+}
+
+interface Movable{
+
+
+}
