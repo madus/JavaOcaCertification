0ecf00416352adf0bab4e5582f8c3af22a6f6c59
madus <madalin.mihai.udrea@gmail.com>
2022-05-29 19:02:51 +0300

    Chapter 1 Java basics Oca

---
+++ Chapter1_JavaBasics/src/executableJavaApplications/HelloExamWithParameters.java
@@ -0,0 +1,12 @@
+package executableJavaApplications;
+
+/**
+ * Created by Alexandra on 5/29/2022.
+ */
+public class HelloExamWithParameters {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+
+}
