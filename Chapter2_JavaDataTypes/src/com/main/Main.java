f879def120b1941b1b7e27829eb6d9dbec92c905
madus <madalin.mihai.udrea@gmail.com>
2022-07-04 13:34:02 +0300

    Chapter2

---
+++ Chapter2_JavaDataTypes/src/com/main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+/**
+ * Created by Alexandra on 7/4/2022.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test Java OCA Chapter 2");
+    }
+}
